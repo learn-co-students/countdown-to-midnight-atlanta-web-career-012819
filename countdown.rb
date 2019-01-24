dc4777348f034fa81540c410a773628faea14137
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-24 20:12:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+def countdown_with_sleep(sec)
+  sleep(5)
 end
+
+
+def countdown(countdown_output)
+  while countdown_output >= 1
+  puts "#{countdown_output} SECOND(S)!"
+  sleep(1)
+  countdown_output -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+countdown(10)
